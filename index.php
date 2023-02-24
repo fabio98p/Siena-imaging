f4c52b0f84090d28d8dba606acf77d1c8832ab78
fabio98p <parisifabio98@gmail.com>
2023-02-24 01:24:05 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siena</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1>html</h1>
+    <?php
+        for ($x = 0; $x <= 5; $x++) {
+            echo "<h1>php: $x </h1><br>";
+        }
+    ?>
+</body>
+<script src="./script.js"></script>
+</html>

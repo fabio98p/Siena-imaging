e7d7e4e0a52563ffd49aacb87ac105ebf6e7dee1
fabio98p <parisifabio98@gmail.com>
2023-02-24 13:44:34 +0100

    create class js with unique id

--- index.php
+++ index.php
@@ -6,29 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Siena</title>
     <link rel="stylesheet" href="./style.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="./script.js"></script>
+
 </head>
 <body>
     <nav>
         <h1>Fabio Parisi</h1>
-        <!-- <button class="add" onclick="add()">+</button>
-        <button class="remove" onclick="remove()">-</button> -->
-        <form id="form" method="post">
+        <button class="add" onclick="add()">+</button>
+        <button class="remove" onclick="newComponent()">-</button>
+        <!-- <form id="form" method="post">
             <input type="submit" name="button1"
                     class="button" value="+" />
             
             <input type="submit" name="button2"
                     class="button" value="-" />
-        </form>
+        </form> -->
     </nav>
     <main>
     <h1>html</h1>
+    <div class="test"></div>
+    <div id="components"></div>
     <?php
         include('script.php');
     ?>
     </main>
 
 </body>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="./script.js"></script>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 console.log("hi");
-// $("#form").submit(function(e) {
-//     e.preventDefault();
-// });
+
+class component {
+    id
+    path
+    constructor() {
+        //add at the component a unique id
+        this.id = component.getNewId();
+
+        //generate the html of the component
+        $("#components").append(`${this.id}`)
+    }
+    
+    //return a unique id
+    static getNewId() {
+        if (!this.latestId) this.latestId = 0;
+            return ++this.latestId;
+    }
+}
+
+let componentList = []
+
+function newComponent(){
+	if (componentList.length < 3) {
+		componentList.push(new component())
+    }
+}
+
+function add(){
+    console.log($(".test")[0]);
+    $(".test")[0].append("gino")
+}
+function remove(){
+    console.log("b");
+
+    $(".test")[0].append("pino") 
+}
